98bae051d264fc58078de89d8e22e094097a6a7e
Angel Restrepo <af.restrepo@uniandes.edu.co>
2021-02-23 13:02:23 -0500

    adicion concurrencia y debug

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,36 +1,41 @@
 package Main;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
 
 
 public class Main {
-	
+
 	private Semaforo[] semaforos;
-	
+
 	private ArrayList<String> buzonesProd;
-	
+
 	public int limitBuzonProd;
-	
+
 	private ArrayList<String> buzonesCons;
-	
+
 	public int limitBuzonCons;
-	
+
 	private String buzonInter;
-	
+
 	public int numProductos;
-	
+
 	private static int nProdCons;
-	
+
 	public ArrayList<String> getBuzonProd() {
 		return buzonesProd;
 	}
-	
+
 	public ArrayList<String> getBuzonCons() {
 		return buzonesCons;
 	}
-	
+
 	public synchronized void addProd(String pProducto) {
 		boolean continuar = true;
 		while (continuar) {
@@ -43,13 +48,12 @@ public class Main {
 			}
 		}		
 	}
-	
+
 	public void getProd(String pProducto) {
 		boolean continuar = true;
-		while (continuar) {
-			semaforos[3].p();
+		while (continuar) {			
 			synchronized(buzonesCons) {
-				if(buzonesCons.contains(pProducto)) {
+				if(buzonesCons.contains(pProducto)) {					
 					for(int i=0; i<buzonesCons.size() && continuar; i++) {
 						if(buzonesCons.get(i).equals(pProducto)) {
 							buzonesCons.remove(i);
@@ -64,7 +68,7 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public void inter1() {
 		int count = 0;		
 		while(count < (numProductos * nProdCons)) {
@@ -80,9 +84,9 @@ public class Main {
 		}
 		System.out.println("fn in");
 	}
-	
+
 	public void inter2() {
-		
+
 		int count = 0;		
 		while(count < (numProductos * nProdCons)) {
 			semaforos[1].p();
@@ -101,7 +105,7 @@ public class Main {
 		}
 		System.out.println("fn out");
 	}
-	
+
 	public Main(int pNumProductos, int pBuzonesProd, int pBuzonesCons) {
 		this.semaforos = new Semaforo[] {new Semaforo(0), new Semaforo(0), new Semaforo(0), new Semaforo(0)};
 		this.buzonesProd = new ArrayList<String>();
@@ -111,12 +115,26 @@ public class Main {
 		this.limitBuzonCons = pBuzonesCons;
 		this.numProductos = pNumProductos; 
 	}
-	
+
 	public static void main(String[] args) {
-		
-		nProdCons = 3;
-		Main main = new Main(2,4,50);
-		
+		Properties concurrencia = new Properties();
+		InputStream configInput = null;	    
+		try{
+			configInput = new FileInputStream("data/concurrencia.properties");
+			concurrencia.load(configInput);
+			System.out.println(concurrencia.getProperty("concurrencia.numProdCons")+" productores y consumidores");
+			System.out.println(concurrencia.getProperty("concurrencia.numProductos")+" productos");
+			System.out.println(concurrencia.getProperty("concurrencia.buzonesProd")+" buzones productores");
+			System.out.println(concurrencia.getProperty("concurrencia.buzonesCons")+" buzones consumidores");
+		} catch(Exception e){
+			JOptionPane.showMessageDialog(null, "Error cargando configuración\n" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+		}		
+		int numProductos = Integer.parseInt (concurrencia.getProperty("concurrencia.numProductos"));
+		int numBuzProd = Integer.parseInt (concurrencia.getProperty("concurrencia.buzonesProd"));
+		int numBuzCons = Integer.parseInt (concurrencia.getProperty("concurrencia.buzonesCons"));
+		nProdCons = Integer.parseInt (concurrencia.getProperty("concurrencia.numProdCons"));
+		Main main = new Main(numProductos,numBuzProd,numBuzCons);
+
 		for(int i=0; i<nProdCons;i++) {
 			if(i<(nProdCons/2)) {
 				new Productor("A"+i, main).start();
@@ -126,9 +144,9 @@ public class Main {
 				new Consumidor("B"+i, main).start();
 			}
 		}
-		
+
 		new Intermediario(1, main).start();
 		new Intermediario(2, main).start();
-		
+
 	}
 }
